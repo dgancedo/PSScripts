c7dadc70fb5b1e66792aa890f6b25982addec758
Diego S. Gancedo <dgancedo@Narsil.local>
2018-05-30 17:11:15 -0300

    PowerCLI VM's and Hosts Invetory added

---
+++ PowerCLI/README.md
@@ -0,0 +1,3 @@
+#PowerCLI Scripts
+
+Simple_VM_Inventory.ps1 #Simple script to inventory Hosts attached to vCenter and VM's
---
+++ PowerCLI/Simple_VM_Inventory.ps1
@@ -0,0 +1,20 @@
+#Description: Simple script to inventory VM's and hosts connected to vCenter
+#Author: dgacendo@trans.com.ar
+#Date: 20180530
+
+Get-Module -ListAvailable PowerCli* | Import-Module
+
+$vc = Read-Host -Prompt "Input vCenter name"
+$creed = Get-Credential -Message "Input vCenter credentials"
+
+Connect-VIServer -Server $vc -Credential $creed
+
+$vmdata = Get-VM -name * | Get-HardDisk | select @{N="VM";E={$_.Parent.Name}},@{N="CPU";E={$_.Parent.numCpu}},@{N="Memory Total MB";E={$_.Parent.memoryMb}},@{N="Provisioned Space";E={$_.Parent.ProvisionedSpaceGB}},@{N="Used Space";E={$_.Parent.UsedSpaceGB}},@{N="Storage GB";E={$_.CapacityGB}}
+
+$hostdata = get-vmhost * | select Name,ConnectionState,Version,Manufacturer,Model,NumCpu,CpuTotalMhz,CpuUsageMhz,MemoryTotalGB,MemoryUsageGB
+foreach ($line in $vmdata){
+    export-csv -InputObject $line -path $vc"_vms.csv" -Append
+}
+foreach ($line in $hostdata){
+    export-csv -InputObject $line -path $vc"_hosts.csv" -Append
+}
