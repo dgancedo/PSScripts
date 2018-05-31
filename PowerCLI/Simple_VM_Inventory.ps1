3796115cceeaf4684d5442a664ee4998fca36093
Diego S. Gancedo <dgancedo@gmail.com>
2018-05-31 12:17:07 -0300

    Change select to select-object

--- PowerCLI/Simple_VM_Inventory.ps1
+++ PowerCLI/Simple_VM_Inventory.ps1
@@ -9,9 +9,9 @@ $creed = Get-Credential -Message "Input vCenter credentials"
 
 Connect-VIServer -Server $vc -Credential $creed
 
-$vmdata = Get-VM -name * | Get-HardDisk | select @{N="VM";E={$_.Parent.Name}},@{N="CPU";E={$_.Parent.numCpu}},@{N="Memory Total MB";E={$_.Parent.memoryMb}},@{N="Provisioned Space";E={$_.Parent.ProvisionedSpaceGB}},@{N="Used Space";E={$_.Parent.UsedSpaceGB}},@{N="Storage GB";E={$_.CapacityGB}}
+$vmdata = Get-VM -name * | Get-HardDisk | Select-Object @{N="VM";E={$_.Parent.Name}},@{N="CPU";E={$_.Parent.numCpu}},@{N="Memory Total MB";E={$_.Parent.memoryMb}},@{N="Provisioned Space";E={$_.Parent.ProvisionedSpaceGB}},@{N="Used Space";E={$_.Parent.UsedSpaceGB}},@{N="Storage GB";E={$_.CapacityGB}}
 
-$hostdata = get-vmhost * | select Name,ConnectionState,Version,Manufacturer,Model,NumCpu,CpuTotalMhz,CpuUsageMhz,MemoryTotalGB,MemoryUsageGB
+$hostdata = get-vmhost * | Select-Object Name,ConnectionState,Version,Manufacturer,Model,NumCpu,CpuTotalMhz,CpuUsageMhz,MemoryTotalGB,MemoryUsageGB
 foreach ($line in $vmdata){
     export-csv -InputObject $line -path $vc"_vms.csv" -Append
 }
